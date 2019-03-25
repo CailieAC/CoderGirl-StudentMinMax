a0bc8e8346a3f80dd52164db5a2055723b8ca573
CailieAC <cailiecarlile@gmail.com>
2019-03-24 23:10:13 -0500

    commit1

--- StudentMinMax/Program.cs
+++ StudentMinMax/Program.cs
@@ -1,17 +1,59 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace StudentMinMax
 {
-    public static class Program
+    public class Program
     {
+        //values for the student class are populated from the included studentdata.txt file. 
+        //The student’s name is the first thing on each line, followed by some exam scores. 
+        //The number of scores might be different for each student.  
+        //The program should calculate the minimum and maximum score for each student and print out their name as well.
         public static void Main()
         {
-            foreach (var student in students)
+            Program program = new Program();
+            program.Run();
+            Console.ReadLine();
+        }
+
+        private void Run()
+        {
+            List<Student> studentList = CreateStudentList();
+
+            foreach (var student in studentList)
             {
                 Console.WriteLine($"{student.Name} Min: {student.GetMinimumScore()} Max: {student.GetMaximumScore()}");
             }
 
             Console.ReadLine();
+        }
+
+        private List<Student> CreateStudentList()
+        {
+            List<Student> studentList = new List<Student>();
+            
+            foreach (string line in File.ReadLines(@"C:\Users\Cailie\Documents\Visual Studio 2017\Projects\CailieAC\CoderGirl-StudentMinMax\studentdata.txt"))
+            {
+                Student student = CreateStudent(line);
+                studentList.Add(student);
+            }
+            return studentList;
+        }
+
+        private Student CreateStudent(string line)
+        {
+            Student student = new Student();
+            string[] properties = line.Split(" ");
+            int[] scores = new int[properties.Length - 1];
+            student.Name = properties[0];
+
+            for (int i = 1; i < properties.Length; i++)
+            {
+                scores[i-1] = int.Parse(properties[i]);
+            }
+            student.Scores = scores;
+            return student;
         }
     }
 }
